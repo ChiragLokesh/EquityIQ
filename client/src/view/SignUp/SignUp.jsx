0aebe739064fac2e640adf7df3f0d67fb49ae937
ChiragLokesh <chiragreddy1@gmail.com>
2024-01-22 12:14:37 +0530

    Logo size changes

--- client/src/view/SignUp/SignUp.jsx
+++ client/src/view/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Login } from '../../services/login';
+import  Logo  from './logo.png';
 
 function SignUpPage() {
   const [username, setUsername] = useState('');
@@ -23,8 +24,8 @@ function SignUpPage() {
     <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-1/4 w-1/4"
+            src={Logo}
             alt="Your Company"
           />
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight dark:text-white text-gray-900">
