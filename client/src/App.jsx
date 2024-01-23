32729b5d9fe52b1efbd9d1f6b9f07f581e975145
ChiragLokesh <chiragreddy1@gmail.com>
2024-01-23 09:45:28 +0530

    forgot password

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import LoginPage from './view/Login/LoginPage'
 import LoginSuccessPage from './view/Login/LoginSuccessPage';
 import SignUpPage from './view/SignUp/SignUp'
 import MarketData from './view/MarketData/MarketData'
+import ForgotPasswordPage from './view/Login/forgotpassword'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
   return (
     <Routes>
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/forgot-password' element={<ForgotPasswordPage/>} />
       <Route path='/sign-up' element={<SignUpPage />} />
       <Route path="/LoginSuccessPage" element={<LoginSuccessPage />} />
       <Route path='/market-data' element={<MarketData />} />
--- client/src/view/Login/forgotpassword.jsx
+++ client/src/view/Login/forgotpassword.jsx
@@ -1,40 +1,18 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
+import { sendPasswordResetEmail } from 'firebase/auth';
 import Logo from './logo.png';
 import { auth } from '../../firebase/firebase';
 
-function LoginPage() {
+function ForgotPasswordPage() {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [okMessage, setOkMessage] = useState('');
   const navigate = useNavigate();
 
-  const emailAndPassword = async (event) => {
+  const handleForgotPassword = async (event) => {
     event.preventDefault();
 
-    try {
-      // Sign in with email/password
-      await signInWithEmailAndPassword(auth, email, password);
-
-      // If successful, you can add your success message here
-      console.log('Login with email/password successful');
-
-      // Now you can navigate or perform additional actions if needed
-      navigate('/LoginSuccessPage');
-    } catch (error) {
-      // If there's an error, you can handle it here
-      console.error('Email/Password Sign-In Error', error);
-
-      // Display an error message to the user
-      setErrorMessage('Failed to sign in. Please check your email and password.');
-
-      // You can use this error message state in your component to show an alert or display the message
-    }
-  };
-
-  const handleForgotPassword = async () => {
     try {
       await sendPasswordResetEmail(auth, email);
       setOkMessage('Password reset email sent. Check your inbox.');
@@ -52,13 +30,13 @@ function LoginPage() {
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img className="mx-auto h-1/4 w-1/4" src={Logo} alt="Your Company" />
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight dark:text-white text-gray-900">
-            Sign in to your account
+            Forgot Password
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
           <div className="bg-white dark:bg-gray-800 px-6 py-12 shadow sm:rounded-lg sm:px-12">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" onSubmit={handleForgotPassword}>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 dark:text-white text-gray-900">
                   Email address
@@ -77,37 +55,11 @@ function LoginPage() {
               </div>
 
               <div>
-                <label htmlFor="password" className="block text-sm font-medium leading-6 dark:text-white text-gray-900">
-                  Password
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="block w-full rounded-md border-0 py-1.5 dark:bg-gray-300 dark:text-gray-900   text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="text-sm leading-6">
-                  <Link to="/forgot-password" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </Link>
-                </div>
-              </div>
-
-              <div>
                 <button
-                  onClick={emailAndPassword}
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Reset Password
                 </button>
               </div>
               {errorMessage && (
@@ -119,10 +71,10 @@ function LoginPage() {
             </form>
           </div>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <Link to="/sign-up" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Create an account today
+          <p className="mt-6 text-center text-sm text-gray-500">
+            Remember your password?{' '}
+            <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Sign in
             </Link>
           </p>
         </div>
@@ -131,4 +83,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default ForgotPasswordPage;
