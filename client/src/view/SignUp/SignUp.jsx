c233cbc31531146421beea244fad6d1a0f219599
ChiragLokesh <chiragreddy1@gmail.com>
2024-02-01 14:12:20 +0530

    Update SignUp.jsx

--- client/src/view/SignUp/SignUp.jsx
+++ client/src/view/SignUp/SignUp.jsx
@@ -132,7 +132,7 @@ function SignUpPage() {
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Already a member?{' '}
-            <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            <Link to="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
               Sign in now
             </Link>
           </p>
