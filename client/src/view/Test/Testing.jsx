b688addb2d22e45771d8bf59eb92daf323cabf6d
ChiragLokesh <chiragreddy1@gmail.com>
2024-03-21 14:42:54 +0530

    padding knowledge hub

--- client/src/view/Test/Testing.jsx
+++ client/src/view/Test/Testing.jsx
@@ -11,34 +11,46 @@ function Testing() {
   };
 
   return (
-    <div className="bg-white pb-28">
-    <div className="heading">
-      <h1 className="text-black text-6xl font-extrabold pt-10 pl-10">Stock Market Education</h1>
-    </div>
-    <div className="pb-10">
-      <h1 className="text-black text-4xl font-bold pl-10">Play the market to your favor</h1>
-    </div>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 p-10">
-      {varsityData.map((data, index) => (
-        <div
-          className="card bg-gray-400 rounded-lg cursor-pointer"
-          onClick={() => openModule(data.num)}
-          key={index}
-        >
-          <div className="container p-4">
-            <h2 className="text-right font-bold text-4xl" style={{ color: data.color }}>
-              <b>{data.num}</b>
-            </h2>
-            <h4 className="text">
-              <b>{data.title}</b>
-            </h4>
-            <p className="text">{data.body}</p>
+    <div className="bg-white min-h-screen">
+      <div className="heading">
+        <h1 className="text-black text-6xl font-extrabold pt-10 pl-10">
+          Stock Market Education
+        </h1>
+      </div>
+      <div className="pb-10">
+        <h1 className="text-black text-4xl font-bold pl-10">
+          Play the market to your favor
+        </h1>
+      </div>
+      <br></br>
+      <br></br>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 p-10">
+        {varsityData.map((data, index) => (
+          <div
+            className="card bg-gray-400 rounded-lg cursor-pointer p-6"
+            onClick={() => openModule(data.num)}
+            key={index}
+          >
+            <div className="container p-4">
+              <h2
+                className="text-right font-bold text-5xl"
+                style={{ color: data.color }}
+              >
+                <b>{data.num}</b>
+              </h2>
+
+              <h4 className="text-2xl">
+                <b>{data.title}</b>
+              </h4>
+
+              <br />
+              <p className="text-lg">{data.body}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default Testing;
